34f3842d7fe6681e8a6189ba6764343bdeeb95b2
kiefu <tono@localhost>
2017-05-08 19:41:45 +0900

    コミット

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -19,14 +19,14 @@ import service.EmployeeService;
 public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
-
+//メイン画面
 		@RequestMapping(value="/employee/{id}",method = RequestMethod.GET)
 		public String Employee(Model model, @PathVariable int id ,String name) {
 		    EmployeeDto Employee = employeeService.getEmployee(id, name);
 		    model.addAttribute("employee", Employee );
 		    return "employee";
 		}
-
+//詳細画面に接続
 		@RequestMapping(value = "/detail/", method = RequestMethod.GET)
 		public String update(Model model,@PathVariable int id,String name) {
 			EmployeeDto employee =employeeService.getEmployee(id, name);
@@ -39,19 +39,21 @@ public class EmployeeController {
 		    model.addAttribute("Dto", Detail);
 		    return "detail";
 		}
+//登録画面
 		@RequestMapping(value = "/register", method = RequestMethod.POST)
 		public String register(@ModelAttribute RegusterDto RegusterDto, Model model) {
 			RegusterDto dto = new RegusterDto();
 		    int count = employeeService.register(dto);
 		    return "redirect:/register/";
 		}
-
+//データの更新
 		@RequestMapping(value = "/update", method = RequestMethod.POST)
 		public String update(@ModelAttribute EmployeeDto Dto, Model model) {
 			EmployeeDto dto = new EmployeeDto();
 		    int count = employeeService.update(dto);
 		    return "redirect:/detail/";
 		}
+//データの削除
 		@RequestMapping(value = "/delete/input/", method = RequestMethod.GET)
 		public String testDelete(Model model) {
 			DetailDto Detail = new DetailDto();
@@ -62,7 +64,7 @@ public class EmployeeController {
 		@RequestMapping(value = "/delete/input/", method = RequestMethod.POST)
 		public String Delete(@ModelAttribute DetailDto form, Model model) {
 		    int count = employeeService.delete(form.getId());
-		    return "redirect:/search/";
+		    return "redirect:/Detail/";
 		}
 }
 
